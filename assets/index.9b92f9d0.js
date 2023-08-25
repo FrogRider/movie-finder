b47a6b53dd4cf4c40ea93c1374c57010cda9c33a
FrogRider <jhonny.lbk@gmail.com>
2023-08-25 20:22:04 +0300

    Updates

---
+++ assets/index.9b92f9d0.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.5518f8e8.js";const t={};function o(r,s){return n(),c("div",null,"details")}const _=e(t,[["render",o]]);export{_ as default};
--- assets/index.b0ee8d72.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./entry.9a8e5fa9.js";const t={};function o(r,s){return n(),c("div",null,"details")}const _=e(t,[["render",o]]);export{_ as default};
